1d2dfc783f950d77a1c79407d9c37c77756445c9
kkamlesh286 <kkamlesh286@gmail.com>
2021-02-26 15:23:50 +0530

    response received

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'dart:convert' show ascii, json, jsonEncode;
+import 'dart:convert' show ascii, base64, json, jsonEncode;
+import 'package:jose/jose.dart';
 
 const SERVER_IP = 'https://amberinfotech.com/dozzbee/api/auth/login';
 final storage = FlutterSecureStorage();
@@ -29,25 +30,15 @@ class LoginPage extends StatelessWidget {
       },
       body: jsonEncode({"username": username, "password": password}),
     );
-    if (res.statusCode == 200)
-    
-     return res.body;
+    if (res.statusCode == 200) return res.body;
     return null;
-    
   }
-  
 
-  // Future<int> attemptSignUp(String username, String password) async {
-  //   var res = await http.post(
-  //     '$SERVER_IP/signup',
-  //     body: {
-  //       "username": username,
-  //       "password": password
-  //     }
-  //   );
-  //   return res.statusCode;
-
-  // }
+  Future<int> attemptSignUp(String username, String password) async {
+    var res = await http.post('$SERVER_IP/signup',
+        body: {"username": username, "password": password});
+    return res.statusCode;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -85,27 +76,34 @@ class LoginPage extends StatelessWidget {
                     }
                   },
                   child: Text("Log In")),
-              // FlatButton(
-              //     onPressed: () async {
-              //       var username = _usernameController.text;
-              //       var password = _passwordController.text;
+              FlatButton(
+                  onPressed: () async {
+                    var username = _usernameController.text;
+                    var password = _passwordController.text;
 
-              //       // if(username.length < 4)
-              //       //   displayDialog(context, "Invalid Username", "The username should be at least 4 characters long");
-              //       // else if(password.length < 4)
-              //       //   displayDialog(context, "Invalid Password", "The password should be at least 4 characters long");
-              //       // else{
-              //       //   var res = await attemptSignUp(username, password);
-              //       //   if(res == 201)
-              //       //     displayDialog(context, "Success", "The user was created. Log in now.");
-              //       //   else if(res == 409)
-              //       //     displayDialog(context, "That username is already registered", "Please try to sign up using another username or log in if you already have an account.");
-              //       //   else {
-              //       //     displayDialog(context, "Error", "An unknown error occurred.");
-              //       //   }
-              //       // }
-              //     },
-              //     child: Text("Sign Up"))
+                    if (username.length < 4)
+                      displayDialog(context, "Invalid Username",
+                          "The username should be at least 4 characters long");
+                    else if (password.length < 4)
+                      displayDialog(context, "Invalid Password",
+                          "The password should be at least 4 characters long");
+                    else {
+                      var res = await attemptSignUp(username, password);
+                      if (res == 201)
+                        displayDialog(context, "Success",
+                            "The user was created. Log in now.");
+                      else if (res == 409)
+                        displayDialog(
+                            context,
+                            "That username is already registered",
+                            "Please try to sign up using another username or log in if you already have an account.");
+                      else {
+                        displayDialog(
+                            context, "Error", "An unknown error occurred.");
+                      }
+                    }
+                  },
+                  child: Text("Sign Up"))
             ],
           ),
         ));
